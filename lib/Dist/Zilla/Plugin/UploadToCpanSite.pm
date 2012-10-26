013f880b679734c132df32c62cf6399514fdd983
Stephen Cardie <stephenca@ls26.net>
2012-10-26 17:27:37 +0100

    Add POD.

--- lib/Dist/Zilla/Plugin/UploadToCpanSite.pm
+++ lib/Dist/Zilla/Plugin/UploadToCpanSite.pm
@@ -30,6 +30,53 @@ has 'cpan'      => ( isa => Str, ro, required, default =>
 # VERSION
 #
 
+=pod
+
+=head1 DESCRIPTION
+
+  ; in dzil.ini
+  [UploadToCpanSite]
+  site        = /var/www/vhost/mycpansite/cpan
+  host        = user@mycpansite.org
+  author      = EXAMPLE
+
+L<CPAN::Site> is a distribution for extending CPAN with private packages.  This
+is a Dist::Zilla::Role::Releaser plugin that uploads a distribution tarball to
+such a site, and regenerates the site indices. It is intended to be used instead of
+L<Dist::Zilla::Plugin::UploadToCPAN>.
+
+=head1 ATTRIBUTES
+
+=head2 site
+
+The base location of the L<CPAN::Site>, i.e. the value of CPANSITE_LOCAL in
+L<CPAN::Site>'s terms.  This is mandatory.
+
+=head2 host
+
+The (user and) hostname at which the L<CPAN::Site> is hosted.  This is
+mandatory.
+
+Note that the user should have write permissions on the filesystem.
+
+=head2 author
+
+The CPAN author ID.  Mandatory.
+
+=head2 directory
+
+The location of the 'authors' directory in the L<CPAN::Site> site.
+Optional.  If not supplied, is built from the 'site' and 'author' attributes,
+e.g. from the sample dist.ini fragment above, the value of directory would be
+'/var/www/vhosts/mycpansite/cpan/authors/id/E/EX/EXAMPLE'.
+
+=head2 cpan
+
+The location of the upstream CPAN archive (i.e. the value of CPANSITE_GLOBAL in L<CPAN::Site>'s
+terms). Optional, defaults to http://ftp.easynet.be/pub/CPAN/.
+
+=cut
+
 sub _build_directory 
 {
     my $self = shift;
